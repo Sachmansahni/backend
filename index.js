45825d007753f8191918f16aa851510442887166
Sachmansahni <raunaksahni71@gmail.com>
2025-09-27 16:46:36 +0530

    accepting data in params and body

--- index.js
+++ index.js
@@ -7,6 +7,19 @@ console.log(port);
 
 console.log("hello")
 
+//befor accepting a request
+//middlewares
+
+//frontend(postman)  -> middleware ->  calls backend (apis)
+app.use(express.json());
+
+app.post('/', (req, res)=>{
+    // console.log("the request is ", req);
+    console.log("the request body is ", req.body);
+    console.log("the query param is ", req.query)
+    res.send("hello world")
+})
+
 app.listen(port , () => {
     // console.log("listening to port number ", port);
     console.log(`listening to port number ${port}`)
